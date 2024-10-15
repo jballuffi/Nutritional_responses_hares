355efd05fcd63c082742df23fd40e76b57af687a
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-10-15 15:12:22 -0600

    run foraging cleaning and save RDS

--- Scripts/06-prep_foraging_data.R
+++ Scripts/06-prep_foraging_data.R
@@ -47,62 +47,21 @@ beh[grid == "Sulphur" | grid == "Rolo" | grid == "Chadbear" | grid == "Leroy", s
 beh[grid == "Chitty", snowgrid := "Agnes"]
 
 
-full[, Moving := Forage + Hopping + Sprinting]
+
+
+ # figures -----------------------------------------------------------------
+
+
+allwinters <-
+  ggplot(beh[m < 5 & m > 1])+
+  geom_point(aes(x = Date, y = Forage/3600))+
+  facet_wrap(~winter, scales = "free")+
+  theme_minimal()
 
 
 
-# # figures -----------------------------------------------------------------
-# 
-# 
-# forg <- ggplot(full)+
-#   geom_point(aes(x = SD, y = Forage), size = 1)+
-#   labs(x = "Snow depth (cm)", y = "Foraging rate (s/day)", title = "Foraging")+
-#   theme_minimal()
-# 
-# mov <- ggplot(full)+
-#   geom_point(aes(x = SD, y = Moving), size = 1)+
-#   labs(x = "Snow depth (cm)", y = "Movement rate (s/day)", title = "Foraging + Hopping + Sprinting")+
-#   theme_minimal()
-# 
-# nonmov <- ggplot(full)+
-#   geom_point(aes(x = SD, y = notmoving), size = 1)+
-#   labs(x = "Snow depth (cm)", y = "Resting rate (s/day)", title = "Not moving")+
-#   theme_minimal()
-# 
-# 
-# 
-# #movbysnow <- ggarrange(forg, mov, nonmov, ncol = 1, nrow = 3)
-# 
-# #ggsave("Output/figures/behavandsnow.jpg", movbysnow, width = 5, height = 9, unit = "in")
-# 
-# 
-# 
-# allwinters <- 
-#   ggplot(full[m < 5 & m > 1])+
-#   geom_point(aes(x = Date, y = Forage/3600))+
-#   facet_wrap(~winter, scales = "free")+
-#   theme_minimal()
-# 
-# 
-# 
-# winter1617 <- 
-#   ggplot()+
-#   geom_point(aes(x = Date, y = Forage/3600), data = beh[winter == "2016-2017"])+
-#   geom_point(aes(x = Date, y = Forage/3600), data = forageday[ winter == "2016-2017"], size = 1.5, color = "red")+
-#   labs(x = "Date", y = "Forage effort (hr/day)", title = "2016-2017")+
-#   theme_minimal()
-# 
-# winter1415 <- 
-#   ggplot()+
-#   geom_point(aes(x = Date, y = Forage/3600), data = beh[winter == "2014-2015"])+
-#   geom_point(aes(x = Date, y = Forage/3600), data = forageday[ winter == "2014-2015"], size = 1.5, color = "red")+
-#   labs(x = "Date", y = "Forage effort (hr/day)", title = "2014-2015")+
-#   theme_minimal()
-# 
-# 
-# ggsave("Output/figures/allwinters.jpg", allwinters, width = 12, height = 10, unit = "in")
-# 
-# ggsave("Output/figures/winter1617.jpg", winter1617, width = 4.5, height = 3.5, unit = "in")
-# 
-# ggsave("Output/figures/winter1415.jpg", winter1415, width = 4.5, height = 3.5, unit = "in")
-# 
+# Save --------------------------------------------------------------------
+
+ggsave("Output/Figures/foraging_allwinters.jpg", allwinters, width = 12, height = 10, unit = "in")
+saveRDS(beh, "Output/Data/foraging_rates.rds")
+
