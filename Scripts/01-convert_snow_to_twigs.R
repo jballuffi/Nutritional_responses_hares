355c4d0d12ca812665c6886c0d4ba10b3e2dd05d
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-10-16 16:13:17 -0600

    snow depth and food figures

--- Scripts/01-convert_snow_to_twigs.R
+++ Scripts/01-convert_snow_to_twigs.R
@@ -26,4 +26,27 @@ food <- merge(snow, predwillow, by = "Snow")
 food <- merge(food, predspruce, by = "Snow")
 
 
+
+food123 <- food[month(Date) == 1 | month(Date) == 2 | month(Date) == 3]
+
+food123 <- food123[!winter == "2014-2015"]
+
+ggplot(food123)+
+  geom_boxplot(aes(x = winter, y = Snow))+
+  labs(y = "mean snow depth (cm)")+
+  theme_minimal()
+
+ggplot(food123)+
+  geom_boxplot(aes(x = winter, y = sbiomass))+
+  labs(y = "Available spruce twigs (g/m2)")+
+  theme_minimal()
+
+ggplot(food123)+
+  geom_boxplot(aes(x = winter, y = wbiomass))+
+  labs(y = "Available willow twigs (g/m2)")+
+  theme_minimal()
+
+
+
+
 saveRDS(food, "Output/Data/snow_and_food.rds")
