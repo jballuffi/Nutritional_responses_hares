6da0bee07d3812431b29c6af1cfb1ff70527d6f9
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-11-19 14:27:08 -0700

    merge snow with final willow prediction

--- Scripts/01-convert_snow_to_twigs.R
+++ Scripts/01-convert_snow_to_twigs.R
@@ -4,16 +4,20 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 #read in data
 snow <- readRDS("Output/Data/snowgrids.rds")
-pred <- readRDS("../Willow_twigs_snowdepth/Output/Data/05_willow_avail_prediction.rds")
+pred <- readRDS("../Willow_twigs_snowdepth/Output/Data/05_willow_biomass_prediction.rds")
 
 
+
+# merge real snow data with prediction for willow availability  -----------
+
+#make snow col lowercase
+setnames(pred, "Snow", "snow")
 
 #round snow depth to nearest cm
-snow[, Snow := round(SD)]
+snow[, snow := round(SD)][, SD := NULL]
 
 #merge food predictions with snow data
-food <- merge(snow, pred, by = "Snow")
-
+food <- merge(snow, pred, by = "snow")
 
 
 food123 <- food[month(Date) == 1 | month(Date) == 2 | month(Date) == 3]
@@ -21,18 +25,13 @@ food123 <- food[month(Date) == 1 | month(Date) == 2 | month(Date) == 3]
 food123 <- food123[!winter == "2014-2015"]
 
 ggplot(food123)+
-  geom_boxplot(aes(x = winter, y = Snow))+
+  geom_boxplot(aes(x = winter, y = snow))+
   labs(y = "mean snow depth (cm)")+
   theme_minimal()
 
 ggplot(food123)+
-  geom_boxplot(aes(x = winter, y = sbiomass))+
-  labs(y = "Available spruce twigs (g/m2)")+
-  theme_minimal()
-
-ggplot(food123)+
-  geom_boxplot(aes(x = winter, y = wbiomass))+
-  labs(y = "Available willow twigs (g/m2)")+
+  geom_boxplot(aes(x = winter, y = biomassavail))+
+  labs(y = "Available willow biomass (g/m2)")+
   theme_minimal()
 
 
