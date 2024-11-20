99e099affdbb5c339e4306d349a3cdb1f9bb1af4
Juliana Balluffi-Fry <jballuffi@gmail.com>
2024-11-19 22:46:09 -0700

    make boxplot

--- Scripts/04-merge_weightchange.R
+++ Scripts/04-merge_weightchange.R
@@ -5,8 +5,7 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 
 density <- readRDS("../HR_PopCycle_SnowshoeHares/output/results/dailyharedensities.rds")
 food <- readRDS("Output/Data/food_adds.rds")
-
-
+wloss <- readRDS("Output/Data/weight_change.rds")
 
 
 
@@ -22,6 +21,18 @@ wloss[is.na(food), food := 0]
 
 wloss[, food := as.factor(food)]
 
-ggplot(wloss)+
-  geom_boxplot(aes(x = winter, y = wchange, color = food))
 
+wlossmeans <- wloss[, .(wchange_mean = mean(wchange, na.rm = TRUE), 
+                        wchange_sd = sd(wchange, na.rm = TRUE)), by = .(food, winter)]
+
+
+
+ggplot(wlossmeans)+
+  geom_errorbar(aes(x = winter, color = food, ymax = wchange_mean + wchange_sd, ymin = wchange_mean - wchange_sd), width = .2)+
+  geom_bar(aes(x = winter, y = wchange_mean, fill = food), position = "dodge", stat = "identity", width = .5)
+   
+ggplot(wloss)+
+  geom_abline(aes(intercept = 0, slope = 0), linetype = 2)+
+  geom_boxplot(aes(x = winter, y = wchange, fill = food), alpha = .7)+
+  themepoints
+  
