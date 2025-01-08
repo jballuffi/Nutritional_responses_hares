237de851dd63cd4c5325e6cd5b27428a793e4d2b
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-01-07 17:50:04 -0700

    get avverages about snow and hare density by winter

--- Scripts/winter_analysis.R
+++ Scripts/winter_analysis.R
@@ -6,20 +6,59 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 density <- readRDS("../HR_PopCycle_SnowshoeHares/output/results/dailyharedensities.rds")
 food <- readRDS("Output/Data/food_adds.rds")
 wloss <- readRDS("Output/Data/weight_change.rds")
+snow <- readRDS("Output/Data/snow_and_food.rds")
 
 
 
 # merge in food add info --------------------------------------------------
 
-setnames(food, c("Eartag", "Food"), c("id", "food"))
-
-food[, id := as.factor(id)]
-
+#merge files
 wloss <- merge(wloss, food, by = c("id", "winter"), all.x = TRUE)
 
-wloss[is.na(food), food := 0]
+#fill in empty food column
+wloss[is.na(food), food := "0"]
 
-wloss[, food := as.factor(food)]
+#remove 2014-2015 winter
+wloss <- wloss[!winter == "2014-2015"]
+
+
+
+# get density and snow stats by winter -------------------------------------------------------
+
+#take the phase and mean density for each winter
+wdensity <- density[, .(phase = getmode(phase), densityavg = mean(haredensity)), winter]
+
+#remove 2014/2015 winter
+snow <- snow[!winter == "2014-2015"]
+
+#use november to march to control for different sampling periods between winters
+snow <- snow[month(Date) > 10 | month(Date) < 4]
+
+#get mean snow depth and willow availability by winter
+wsnow <- snow[, .(snowavg = mean(snow), biomassavg = mean(biomassavail)), winter]
+
+#get daily snow across grids
+dsnow <- snow[, .(snow = mean(snow)), .(Date, winter)]
+
+ggplot(dsnow)+
+  geom_line(aes(x = Date, y = snow))+
+  facet_wrap(~winter, scales = "free")
+
+#get number of days where snow was greater than 45 cm by winter
+deepdays <- dsnow[snow > 45, .(deepdays = .N), winter]
+
+#merge deep days with averages
+wsnow <- merge(wsnow, deepdays, by = "winter", all.x = TRUE)
+
+#where deep days is NA make 0
+wsnow[is.na(deepdays), deepdays := 0]
+
+
+
+# merge in winter stats with weight loss ----------------------------------
+
+
+
 
 
 wlossmeans <- wloss[, .(wchange_mean = mean(wchange, na.rm = TRUE), 
