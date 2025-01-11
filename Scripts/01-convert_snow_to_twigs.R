f065baa934524c3431bcffbc105757f7a6fe7784
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-01-10 17:12:09 -0700

    make a new RDS with just annual snow info

--- Scripts/01-convert_snow_to_twigs.R
+++ Scripts/01-convert_snow_to_twigs.R
@@ -24,20 +24,54 @@ food <- merge(snow, pred, by = "snow")
 
 food[, Date := ymd(Date)]
 
-food123 <- food[month(Date) == 1 | month(Date) == 2 | month(Date) == 3]
+food <- food[!winter == "2014-2015"]
 
-food123 <- food123[!winter == "2014-2015"]
+#use november to march to control for different sampling periods between winters
+food <- food[month(Date) > 10 | month(Date) < 4]
 
-ggplot(food123)+
+
+
+# Get info for each winter and snow grid ------------------------------------------------
+
+#get mean snow depth and willow availability by winter
+wsnow <- food[, .(snowavg = mean(snow), snowmax = max(snow), biomassavg = mean(biomassavail)), by = .(winter, snowgrid)]
+
+#get daily snow across grids
+dsnow <- food[, .(snow = mean(snow)), .(Date, winter, snowgrid)]
+
+#get number of days where snow was greater than 45 cm by winter
+deepdays <- dsnow[snow > 45, .(deepdays = .N), by = .(winter, snowgrid)]
+
+#merge deep days with averages
+allwsnow <- merge(wsnow, deepdays, by = c("winter", "snowgrid"), all.x = TRUE)
+
+#where deep days is NA make 0
+allwsnow[is.na(deepdays), deepdays := "0"]
+
+
+
+
+# Figures and save --------------------------------------------------------
+
+
+snowplot <- 
+  ggplot(dsnow)+
+  geom_line(aes(x = Date, y = snow, color = snowgrid))+
+  labs(y = "Snow depth (cm)")+
+  facet_wrap(~winter, scales = "free")+
+  themepoints
+
+ggplot(food)+
   geom_boxplot(aes(x = winter, y = snow))+
   labs(y = "mean snow depth (cm)")+
   theme_minimal()
 
-ggplot(food123)+
+ggplot(food)+
   geom_boxplot(aes(x = winter, y = biomassavail))+
   labs(y = "Available willow biomass (g/m2)")+
   theme_minimal()
 
 
-
+ggsave("Output/Figures/snow_annual.jpeg", snowplot, width = 10, height = 8, unit = "in")
+saveRDS(allwsnow, "Output/Data/annual_snow_conditions.rds")
 saveRDS(food, "Output/Data/snow_and_food.rds")
