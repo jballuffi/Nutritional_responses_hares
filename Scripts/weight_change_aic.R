31e2d93ced69a714e4b40fb438f5090f04eb7dc3
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-01-13 11:09:29 -0700

    start aic

--- Scripts/weight_change_aic.R
+++ Scripts/weight_change_aic.R
@@ -20,8 +20,41 @@ w <- merge(w, densitya, by = "winter", all.x = TRUE)
 w <- merge(w, snow, by = c("winter", "snowgrid"), all.x = TRUE )
 
 
+
+# make models to compare with AIC -----------------------------------------
+
+#food add, snow depth, food availability, hare density, lynx density, phase
+#snowgrid, winter
+
+
+n <- lm(weight.c.resid ~ 1, w)
+f <- lm(weight.c.resid ~ food, w)
+s <- lm(weight.c.resid ~ snowavg, w)
+t <- lm(weight.c.resid ~ biomassavg, w)
+h <- lm(weight.c.resid ~ hares.avg, w)
+l <- lm(weight.c.resid ~ lynx, w)
+p <- lm(weight.c.resid ~ phase, w)
+
+
+
+mods <- list(n, f, s, t, h, l, p)
+
+Names <- c('n', 'f', 's', 't', 'h', 'l', 'p')
+
+AIC <- as.data.table(aictab(REML = F, cand.set = mods, modnames = Names, sort = TRUE))
+
+AIC[, ModelLik := NULL]
+
+AIC[, Cum.Wt := NULL]
+
+#round whole table to 3 dec places
+AIC <- AIC %>% mutate_if(is.numeric, round, digits = 3)
+
+
+
 ggplot(w)+
-  geom_point(aes(x = snowmax, y = weight.c.resid, color = food))
+  geom_point(aes(x = snowavg, y = weight.c.resid, color = food))+
+  geom_smooth(aes(x = snowavg, y = weight.c.resid, color = food), method = "lm")
 
 ggplot(w)+
   geom_boxplot(aes(x = winter, y = weight.c.resid, color = food))
