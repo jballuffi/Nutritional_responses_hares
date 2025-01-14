f370cc9ab9eb7f296a8d2b36b93d3dd6bb4c6112
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-01-14 15:09:09 -0700

    clean up foraging prep

--- Scripts/08-foraging_prep.R
+++ Scripts/08-foraging_prep.R
@@ -5,8 +5,8 @@
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
 beh <- fread("Input/allHareDailyValues2015_2021.csv")
-trapping <- fread("Input/Trapping_data_all_records.csv")
-
+#trapping <- fread("Input/Trapping_data_all_records.csv")
+inds <- readRDS("Output/Data/individual_info.rds")
 
 
 # foraging data -----------------------------------------------------------
@@ -28,24 +28,8 @@ beh[m > 8, winter := paste0(y, "-", y+1)]
 #swap B's in the collar data for 2's
 beh[, id := gsub("B", "2", id)]
 
-#pull out the grid of every individual using the trapping data
-trapping[, Eartag := as.character(Eartag)]
-grids <- trapping[, getmode(grid), Eartag]
-names(grids) <- c("id", "grid")
-
 #merge grids into behaviour data set
-beh <- merge(beh, grids, by = "id", all.x = TRUE)
-
-#now we need to match grid in behaviour data set to the snow data set.
-#more grids with collar data than grids with snow data
-
-#when grid with bunny is one of the snow grids, just copy to new col snowgrid
-beh[grid == "Agnes" | grid == "Kloo" | grid == "Jo", snowgrid := grid]
-
-#all other grids and their closest snow grid, but where is leroy?
-beh[grid == "Sulphur" | grid == "Rolo" | grid == "Chadbear" | grid == "Leroy", snowgrid := "Kloo"]
-beh[grid == "Chitty", snowgrid := "Agnes"]
-
+beh <- merge(beh, inds, by = "id", all.x = TRUE)
 
 
 
