4b99786eb8f030853c2e3ff9cad88c5083dcba76
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-01-25 16:17:25 -0700

    save winter, monthly, and daily foraging rates

--- Scripts/08-foraging_prep.R
+++ Scripts/08-foraging_prep.R
@@ -9,11 +9,13 @@ beh <- fread("Input/allHareDailyValues2015_2021.csv")
 inds <- readRDS("Output/Data/individual_info.rds")
 
 
+
 # foraging data -----------------------------------------------------------
 
 #create year and month column
 beh[, m := month(Date)]
 beh[, y := year(Date)]
+beh[, date := ymd(Date)]
 
 #name the winter months
 wintermonths <- c(1, 2, 3, 11, 12)
@@ -28,18 +30,32 @@ beh[m > 8, winter := paste0(y, "-", y+1)]
 #swap B's in the collar data for 2's
 beh[, id := gsub("B", "2", id)]
 
+#take only main cols and convert foraging to hours
+beh <- beh[, .(winter, id, m, y, date, forage = Forage/3600)]
+
+
+
+# Get foraging by month and winter ----------------------------------------
+
+behwinter <- dat[, .(forage = mean(forage)), by = .(winter, id)]
+
+behmonth <- dat[, .(forage = mean(forage)), by = .(winter, m, id)]
+
+
 #merge grids into behaviour data set
-beh <- merge(beh, inds, by = "id", all.x = TRUE)
+dat <- merge(beh, inds, by = c("id", "winter"), all.x = TRUE)
+datwinter <- merge(behwinter, inds, by = c("id", "winter"), all.x = TRUE)
+datmonth <- merge(behmonth, inds, by = c("id", "winter"), all.x = TRUE)
 
 
 
- # figures -----------------------------------------------------------------
-
+# figures -----------------------------------------------------------------
 
 allwinters <-
-  ggplot(beh[m < 5 & m > 1])+
-  geom_point(aes(x = Date, y = Forage/3600))+
+  ggplot(dat[m < 5 & m > 1])+
+  geom_point(aes(x = date, y = forage))+
   facet_wrap(~winter, scales = "free")+
+  labs(x = "Date", y = "Daily foraging effort (hr)")+
   theme_minimal()
 
 
@@ -47,5 +63,9 @@ allwinters <-
 # Save --------------------------------------------------------------------
 
 ggsave("Output/Figures/foraging_allwinters.jpg", allwinters, width = 12, height = 10, unit = "in")
-saveRDS(beh, "Output/Data/foraging_rates.rds")
+
+
+saveRDS(dat, "Output/Data/foraging_daily.rds")
+saveRDS(datmonth, "Output/Data/foraging_monthly.rds")
+saveRDS(datwinter, "Output/Data/foraging_winter.rds")
 
