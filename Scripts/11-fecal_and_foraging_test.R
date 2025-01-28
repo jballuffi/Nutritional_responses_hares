b96dade6cc59e3763b6e546dc0625e605bb92093
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-01-28 16:48:22 -0700

    analysis between foraging and feces and weight change

--- Scripts/11-fecal_and_foraging_test.R
+++ Scripts/11-fecal_and_foraging_test.R
@@ -19,17 +19,20 @@ fecal <- readRDS("Output/Data/fecal_protein.rds")
 
 
 
-# Does foraging rate correlate with fecal protein -------------------------
+# Does foraging rate correlate with fecal protein? -------------------------
 
 #merge monthly foraging rates with fecal sample from the same month
 fecal_m <- merge(fecal, forag_m, by = c("id", "winter", "m", "snowgrid", "food", "sex"), all.x = TRUE)
 
 ggplot(fecal_m)+
-  geom_point(aes(x = forage, y = CP_dm))+
-  geom_smooth(aes(x = forage, y = CP_dm), method = "lm")+
+  geom_point(aes(x = forage, y = CP_dm, color = food))+
+  geom_smooth(aes(x = forage, y = CP_dm, color = food, fill = food), method = "lm")+
+  scale_color_manual(values = foodcols)+
+  scale_fill_manual(values = foodcols)+
   labs(x = "Monthly foraging rate (hr/day)", y = "Fecal protein (%)")+
   themepoints
 
+summary(lm(CP_dm ~ forage*food, fecal_m))
 
 #pull dates of each fecal sample, subtract one to get the day before
 #create a column called "used for daily"
@@ -47,12 +50,52 @@ fecal_d <- merge(fecal, forag_d_fecal, by = c("id", "date"), all.x = TRUE)
 
 
 ggplot(fecal_d)+
-  geom_point(aes(x = prev.dforag, y = CP_dm))+
-  geom_smooth(aes(x = prev.dforag, y = CP_dm), method = "lm")+
+  geom_point(aes(x = prev.dforag, y = CP_dm, color = food))+
+  geom_smooth(aes(x = prev.dforag, y = CP_dm, color = food, fill = food), method = "lm")+
+  scale_color_manual(values = foodcols)+
+  scale_fill_manual(values = foodcols)+
   labs(x = "Foraging rate of previous day (hr/day)", y = "Fecal protein (%)")+
   themepoints
 
+summary(lm(CP_dm ~ prev.dforag*food, fecal_d))
 
-summary(lm(CP_dm ~ forage, fecal_m))
-summary(lm(CP_dm ~ prev.dforag, fecal_d))
+
+
+# Does foraging rates and fecal protein predict weight change? ---------------------
+
+
+weights <- merge(weights, forag_w, by = c("id", "winter", "sex", "food", "snowgrid"), all.x = TRUE)
+
+#get mean fecal protein by ind and winter
+meanfecal <- fecal[, .(CP_dm_avg = mean(CP_dm)), by = .(id, winter)]
+
+weights <- merge(weights, meanfecal, by = c("id", "winter"), all.x = TRUE)
+
+
+ggplot(weights)+
+  geom_abline(intercept = 0, slope = 0, linetype = 2, color = "grey30", linewidth = 0.75)+
+  geom_point(aes(x = CP_dm_avg, y = weight.c.resid, color = food))+
+  geom_smooth(aes(x = CP_dm_avg, y = weight.c.resid, color = food, fill = food), method = "lm")+
+  scale_color_manual(values = foodcols)+
+  scale_fill_manual(values = foodcols)+
+  labs(x = "Fecal protein (%)", y = "Weight change residual (g)")+
+  themepoints
+
+summary(lm(weight.c.resid ~ CP_dm_avg*food, weights))
+
+
+
+
+ggplot(weights)+
+  geom_abline(intercept = 0, slope = 0, linetype = 2, color = "grey30", linewidth = 0.75)+
+  geom_point(aes(x = forage, y = weight.c.resid, color = food))+
+  geom_smooth(aes(x = forage, y = weight.c.resid, color = food, fill = food), method = "lm")+
+  scale_color_manual(values = foodcols)+
+  scale_fill_manual(values = foodcols)+
+  labs(x = "Average foraging effort (hr/day)", y = "Weight change residual (g)")+
+  themepoints
+
+summary(lm(weight.c.resid ~ forage*food, weights))
+
+
 
