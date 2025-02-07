82b5e351c0cef6266a755c3f55057d85d7f73b2c
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-02-07 16:28:25 -0700

    add new theme with smaller font

--- R/ggthemes.R
+++ R/ggthemes.R
@@ -17,7 +17,25 @@ themepoints <- theme(text = element_text(family = "serif"),
                      panel.border = element_blank(),
                      panel.grid.major = element_line(size = 0.5, color = "grey90"))
 
-themepointstop <- theme(text = element_text(family = "serif"),
+themepoints_small <- theme(text = element_text(family = "serif"),
+                     title = element_text(family = "serif"),
+                     axis.title = element_text(size=12, family = "serif"),
+                     axis.text = element_text(size=8, family = "serif"),
+                     legend.position = "right",
+                     legend.key = element_blank(),
+                     #legend.title = element_blank(),
+                     panel.background = element_blank(),
+                     strip.background = element_blank(),
+                     axis.line.x.top = element_blank(),
+                     axis.line.y.right = element_blank(),
+                     axis.line.x.bottom = element_line(size=.5),
+                     axis.line.y.left = element_line(size=.5),
+                     panel.border = element_blank(),
+                     panel.grid.major = element_line(size = 0.5, color = "grey90"))
+
+
+
+themepoints_top <- theme(text = element_text(family = "serif"),
                      title = element_text(family = "serif"),
                      axis.title = element_text(size=14, family = "serif"),
                      axis.text = element_text(size=12, family = "serif"),
