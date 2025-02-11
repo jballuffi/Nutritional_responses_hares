0d5c28cdb0d171f8a1dd2ece26343ea2fe58064b
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-02-10 18:26:44 -0700

    make mixed models for AIC

--- Scripts/11-foraging_aic.R
+++ Scripts/11-foraging_aic.R
@@ -1,7 +1,7 @@
 
 #script to assess how environmental factors impact foraging rates on a winter and daily basis
 
-#TO DO: mixed models for daily. 
+#TO DO: mixed models for daily.
 #To do: make a full AIC function to shorten script
 #To do: whats the deal with sex?
 
@@ -9,13 +9,13 @@
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
 forag <- readRDS("Output/Data/foraging_weekly.rds")
-datweek <- readRDS("Output/Data/full_data_weekly.rds")
+dat <- readRDS("Output/Data/full_data_weekly.rds")
 
 
 
 # merge info and make a control and food add dataset --------
 
-forag <- merge(forag, datweek, by = c("year", "yearfactor", "week"), all.x = TRUE)
+forag <- merge(forag, dat, by = c("year", "yearfactor", "week"), all.x = TRUE)
 
 #get controls only
 foragcon <- forag[food == 0]
@@ -31,28 +31,35 @@ summary(lm(forage ~ sex, data = foragcon))
 
 
 
+# look at potential correlations within data ------------------------------
+
+#correlations: biomass and quality
+#              hare density and per cap
+
+cor(dat$biomass, dat$mortrate)
+
+plot(foragcon$percap ~ foragcon$mortrate)
+
+
+
 # AIC to explain weekly foraging for controls only ------------------------
 
 #models for controls only
-n <- lm(forage ~ 1, foragcon)
-
-b <- lm(forage ~ biomass + temp + sex, foragcon)
-pc <- lm(forage ~ percap + temp + sex, foragcon)
-q <- lm(forage ~ quality + temp + sex, foragcon)
-
-h <- lm(forage ~ haredensity + temp + sex, foragcon)
-m <- lm(forage ~ mortrate + temp + sex, foragcon)
-#t <- lm(forage ~ temp + sex, foragcon)
+n <- lmer(forage ~ 1 + (1|id), foragcon) #null model
+st <- lmer(forage ~ temp + sex + mortrate + (1|id), foragcon) #base model: temp and sex and mortality
+b <- lmer(forage ~ biomass + temp + sex + mortrate + (1|id), foragcon) #biomass food
+pc <- lmer(forage ~ percap + temp + sex + mortrate + (1|id), foragcon) #percapita food
+q <- lmer(forage ~ quality + temp + sex + mortrate + (1|id), foragcon) #quality food
+h <- lmer(forage ~ haredensity + temp + sex + mortrate + (1|id), foragcon) #hare density
 
 #list models
-mods <- list(n, b, pc, q, h, m)
+mods <- list(n, st, b, pc, q, h)
 
 #name models
-Names <- c('Null', 'Biomass', 'PerCap', 'Quality', 'Hares', 'Mortality')
+Names <- c('Null', 'Base', 'Biomass', 'PerCap', 'Quality', 'Hares')
 
 #make AIC table
 AICcon <- as.data.table(aictab(REML = F, cand.set = mods, modnames = Names, sort = TRUE))
-summary(lm(forage ~ sex, data = foragcon))
 
 #remove unwanted columns
 AICcon[, ModelLik := NULL]
@@ -62,9 +69,8 @@ AICcon[, Cum.Wt := NULL]
 AICcon <- AICcon %>% mutate_if(is.numeric, round, digits = 3)
 
 #run function and get R2s for all models
-R2scon <- lapply(mods, collectR2)
+R2scon <- lapply(mods, collectR2_mixed)
 R2scon <- rbindlist(R2scon, fill = TRUE)
-setnames(R2scon, "V1", "R2")
 R2scon$Modnames <- Names
 
 #merge R2s with AIC table
@@ -73,38 +79,46 @@ setorder(AICcon, "Delta_AICc")
 
 
 
-#most parsimonious model is the percapita and biomass
-summary(pc)
-pc_pred <- as.data.table(ggpredict(pc, terms = "percap"))
+summary(q)
+q_pred <- as.data.table(ggpredict(q, terms = "quality"))
 
-(pc_fig <- 
-  ggplot()+
-  geom_point(aes(x = percap, y = forage), alpha = .3, data = foragcon)+
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = pc_pred)+
-  geom_line(aes(x = x, y = predicted), data = pc_pred)+
-  labs(x = "Weekly twig availability (kg/hare)", y = "Weekly foraging effort (hr/day)")+
-  themepoints)
+(q_fig <- 
+    ggplot()+
+    geom_point(aes(x = quality, y = forage), alpha = .3, data = foragcon)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = q_pred)+
+    geom_line(aes(x = x, y = predicted), data = q_pred)+
+    labs(x = "Twig solubility (%)", y = "Weekly foraging effort (hr/day)")+
+    themepoints)
+
+summary(b)
+b_pred <- as.data.table(ggpredict(b, terms = "biomass"))
+
+(q_fig <- 
+    ggplot()+
+    geom_point(aes(x = biomass, y = forage), alpha = .3, data = foragcon)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = b_pred)+
+    geom_line(aes(x = x, y = predicted), data = b_pred)+
+    labs(x = "Twig biomass (hare/ha)", y = "Weekly foraging effort (hr/day)")+
+    themepoints)
 
 
 
 
 # AIC to explain weekly foraging in food add data set ------------------------------------------------
 
-foodn <- lm(forage ~ 1, foragfood)
-foodf <- lm(forage ~ food + temp, foragfood)
-foodb <- lm(forage ~ food*biomass + temp, foragfood)
-foodpc <- lm(forage ~ food*percap + temp, foragfood)
-foodh <- lm(forage ~ food*haredensity + temp, foragfood)
-foodm <- lm(forage ~ food*mortrate + temp, foragfood)
-#foodte <- lm(forage ~ food*temp, foragfood)
+#models for controls only
+foodnull <- lmer(forage ~ 1 + (1|id), foragfood) #null model
+foodbase <- lmer(forage ~ mortrate*food + food + temp + sex + (1|id), foragfood) #base model: temp and sex and mortality
+foodb <- lmer(forage ~ biomass*food + temp + sex + mortrate + (1|id), foragfood) #biomass food
+foodpc <- lmer(forage ~ percap*food + temp + sex + mortrate + (1|id), foragfood) #percapita food
+foodq <- lmer(forage ~ quality*food + temp + sex + mortrate + (1|id), foragfood) #quality food
+foodh <- lmer(forage ~ haredensity*food + temp + sex + mortrate + (1|id), foragfood) #hare density
 
 #list models
-modsfood <- list(foodn, foodf, foodb, foodpc, 
-                 foodh, foodm)
+modsfood <- list(foodnull, foodbase, foodb, foodpc, foodq, foodh)
 
 #name models
-Namesfood <- c('Null', 'Food', 'Food*Biomass', 'Food*PerCap',
-               'Food*Hares', 'Food*Mortality')
+Namesfood <- c('Null', 'Base', 'Food*Biomass', 'Food*PerCap', 'Food*Quality', 'Food*Hares')
 
 #make AIC table
 AICfood <- as.data.table(aictab(REML = F, cand.set = modsfood, modnames = Namesfood, sort = TRUE))
@@ -117,14 +131,29 @@ AICfood[, Cum.Wt := NULL]
 AICfood <- AICfood %>% mutate_if(is.numeric, round, digits = 3)
 
 #run function and get R2s for all models
-R2sfood <- lapply(modsfood, collectR2)
+R2sfood <- lapply(modsfood, collectR2_mixed)
 R2sfood <- rbindlist(R2sfood, fill = TRUE)
-setnames(R2sfood, "V1", "R2")
 R2sfood$Modnames <- Namesfood
 
 #merge R2s with AIC table
 AICfood <- merge(AICfood, R2sfood, by = "Modnames")
 setorder(AICfood, "Delta_AICc")
+
+
+#not far behind = per cap
+foodh_pred <- as.data.table(ggpredict(foodh, terms = c("haredensity", "food")))
+setnames(foodh_pred, "group", "food")
+
+(foodh_fig <- 
+    ggplot()+
+    geom_point(aes(x = haredensity, y = forage, color = food), alpha = .2, data = foragfood)+
+    geom_ribbon(aes(x = x, ymax = conf.high, ymin = conf.low, fill = food), alpha = .5, data = foodh_pred)+
+    geom_line(aes(x = x, y = predicted, color = food), data = foodh_pred)+
+    scale_color_manual(values = foodcols)+
+    scale_fill_manual(values = foodcols)+
+    labs(x = "Hare density (hare/ha)", y = "Weekly foraging effort (hr)")+
+    themepoints)
+
 
 #not far behind = per cap
 foodpc_pred <- as.data.table(ggpredict(foodpc, terms = c("percap", "food")))
