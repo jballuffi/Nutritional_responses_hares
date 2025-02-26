e5eb7de6b960b4645cdd06015c6156cb8cfa99f9
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-02-26 14:47:06 -0700

    re order panels

--- Scripts/07-summary_figures.R
+++ Scripts/07-summary_figures.R
@@ -37,42 +37,42 @@ sumdepfig <- ggarrange(feces, foraging, nrow = 2, ncol = 1)
 
 # Non-food variables ------------------------------------------------------
 
-#temperature
-(tweek <- 
+#hare density
+(dweek <- 
    ggplot(dat)+
-   geom_abline(intercept = median(dat$temp, na.rm = TRUE), slope = 0, linetype = 2)+
-   geom_line(aes(x = date, y = temp), linewidth = .8)+
-   labs(y = "Temperature (C)", x = "", title = "A)")+
+   geom_abline(intercept = median(dat$haredensity, na.rm = TRUE), slope = 0, linetype = 2)+
+   geom_line(aes(x = date, y = haredensity), linewidth = .8)+
+   labs(y = "Hare Density (hares/ha)", x = "", title = "A)")+
    facet_wrap(~year, scales = "free_x")+
    themepoints_small)
 
 #mortality rate
 (mweek <- 
+    ggplot(dat)+
+    geom_abline(intercept = median(dat$mortrate, na.rm = TRUE), slope = 0, linetype = 2)+
+    geom_line(aes(x = date, y = mortrate), linewidth = .8)+
+    labs(y = "Mortality rate", x = "", title = "B)")+
+    facet_wrap(~year, scales = "free_x")+
+    themepoints_small)
+
+#temperature
+(tweek <- 
    ggplot(dat)+
-   geom_abline(intercept = median(dat$mortrate, na.rm = TRUE), slope = 0, linetype = 2)+
-   geom_line(aes(x = date, y = mortrate), linewidth = .8)+
-   labs(y = "Mortality rate", x = "", title = "B)")+
+   geom_abline(intercept = median(dat$temp, na.rm = TRUE), slope = 0, linetype = 2)+
+   geom_line(aes(x = date, y = temp), linewidth = .8)+
+   labs(y = "Temperature (C)", x = "", title = "C)")+
    facet_wrap(~year, scales = "free_x")+
    themepoints_small)
+
 
 #snow depth 
 (sweek <- 
    ggplot(dat)+
    geom_abline(intercept = median(dat$snow, na.rm = TRUE), slope = 0, linetype = 2)+
    geom_line(aes(x = date, y = snow), linewidth = .8)+
-   labs(y = "Snow depth (cm)", x = "", title = "C)")+
+   labs(y = "Snow depth (cm)", x = "", title = "D)")+
    facet_wrap(~year, scales = "free_x")+
    themepoints_small)
-
-#hare density
-(dweek <- 
-    ggplot(dat)+
-    geom_abline(intercept = median(dat$haredensity, na.rm = TRUE), slope = 0, linetype = 2)+
-    geom_line(aes(x = date, y = haredensity), linewidth = .8)+
-    labs(y = "Hare Density (hares/ha)", x = "", title = "D)")+
-    facet_wrap(~year, scales = "free_x")+
-    themepoints_small)
-
 
 
 
@@ -96,7 +96,7 @@ sumdepfig <- ggarrange(feces, foraging, nrow = 2, ncol = 1)
     facet_wrap(~year, scales = "free_x")+
     themepoints_small)
 
-sumindfig <- ggarrange(tweek, mweek, sweek, dweek, bweek, pcweek, ncol = 2, nrow = 3)
+sumindfig <- ggarrange(dweek, mweek, tweek, sweek, bweek, pcweek, ncol = 2, nrow = 3)
 
 
 
