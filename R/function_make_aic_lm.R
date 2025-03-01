6a037df79def8c06b12850bf4581972925564b08
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-02-28 19:00:04 -0700

    function to make AIC with lm

---
+++ R/function_make_aic_lm.R
@@ -0,0 +1,28 @@
+
+#function that makes AIC table with R2s from linear models (lm made models)
+#modlist = supply a list of models
+#modnames = supply a list of names that matches the models
+
+
+make_aic_lm <- function(modlist, modnames){
+  #make AIC table
+  aic <- as.data.table(aictab(cand.set = modlist, sort = TRUE, modnames = modnames))
+  
+  #remove unwanted columns
+  aic[, ModelLik := NULL]
+  aic[, Cum.Wt := NULL]
+  
+  #round whole table to 3 dec places
+  aic <- aic %>% mutate_if(is.numeric, round, digits = 3)
+  
+  #run function and get R2s for all models
+  R2s <- lapply(modlist, collectR2)
+  R2s <- rbindlist(R2s, fill = TRUE)
+  R2s$Modnames <- modnames
+  
+  #merge R2s with AIC table
+  aic <- merge(aic, R2s, by = "Modnames")
+  setorder(aic, "Delta_AICc")
+  
+  return(aic)
+}
