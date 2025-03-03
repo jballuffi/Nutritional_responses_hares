ecc0417d19bd65dd1a0915fe897f76d06528ac54
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-03-03 15:35:15 -0700

    make post hoc figure

--- Scripts/explore_foraging rate.R
+++ Scripts/explore_foraging rate.R
@@ -15,20 +15,46 @@ forag <- merge(forag, dat, by = c("week", "year", "yearfactor"), all.x = TRUE)
 #get controls only
 foragcon <- forag[food == 0]
 
-#take only the years where there was food add
-foodyears <- forag[food == 1, unique(winter)]
-
-#take only females for food add comparisons
-foragfood <- forag[winter %in% foodyears & sex == "female"]
 
 
-ggplot(foragcon[year == 2017])+
-  geom_path(aes(x = date, y = forage, group = id))+
-  geom_point(aes(x = date, y = forage))+
-  theme_bw()
+# Post hoc figure  --------------------------------------------------------
 
-ggplot(dat[year == 2017])+
-  geom_path(aes(x = date, y = temp, group = 1))+
-  geom_point(aes(x = date, y = temp))+
-  theme_bw()
+#foraging and temperature
+#2018 had fluctuating temperature and a sudden drop in available biomass
+#and we can see week to week fluctuation in foraging rates with an overall decline once biomass
+#drops in availability
+
+fortemp <- ggplot(foragcon[year == 2017])+
+  geom_path(aes(x = date, y = forage, group = id), alpha = 0.3)+
+  geom_point(aes(x = date, y = forage, color = temp), size = 2)+
+  scale_color_gradient(low = "skyblue", high = "darkblue", guide = NULL)+
+  labs(x = "Date", y = "Foraging effort (hr/day)", subtitle = "A)")+
+  themepoints
+
+forbio <- ggplot(foragcon[year == 2017])+
+  geom_path(aes(x = date, y = forage, group = id), alpha = 0.3)+
+  geom_point(aes(x = date, y = forage, color = biomass), size = 2)+
+  scale_color_gradient(low = "lightgreen", high = "darkgreen", guide = NULL)+
+  labs(x = "Date", y = "Foraging effort (hr/day)", subtitle = "B)")+
+  themepoints
+
+temp <- ggplot(dat[year == 2017])+
+  geom_path(aes(x = date, y = temp, group = 1), alpha = 0.3)+
+  geom_point(aes(x = date, y = temp, color = temp), size = 2)+
+  scale_color_gradient(low = "skyblue", high = "darkblue", guide = NULL)+
+  labs(x = "Date", y = "Temperature (C)", subtitle = "C)")+
+  themepoints
+
+biomass <- ggplot(dat[year == 2017])+
+  geom_path(aes(x = date, y = biomass, group = 1), alpha = 0.3)+
+  geom_point(aes(x = date, y = biomass, color = biomass), size = 2)+
+  scale_color_gradient(low = "lightgreen", high = "darkgreen", guide = NULL)+
+  labs(x = "Date", y = "Soluble biomss (kg/ha)", subtitle = "D")+
+  themepoints
+
+
+(fullplot <- ggarrange(fortemp, forbio, temp, biomass, ncol = 2, nrow = 2))
+
+
+ggsave("Output/Figures/PostHoc_figure.jpeg", fullplot, width = 7, height = 7, unit = "in")
 
