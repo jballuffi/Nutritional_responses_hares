96a9250623ca275cc59ff9a15f28288e2c210fd2
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-03-08 15:35:49 -0700

    improve appearance of data for conceptual diagram, start foraging vs fecal test

--- Scripts/explore_foraging rate.R
+++ Scripts/explore_foraging rate.R
@@ -2,6 +2,7 @@
 #source the R folder to load any packages and functions
 lapply(dir('R', '*.R', full.names = TRUE), source)
 
+fecal <- readRDS("Output/Data/fecal_protein.rds")
 forag <- readRDS("Output/Data/foraging_weekly.rds")
 dat <- readRDS("Output/Data/full_data_weekly_nogrid.rds")
 
@@ -10,23 +11,45 @@ dat <- readRDS("Output/Data/full_data_weekly_nogrid.rds")
 # merge info and make a control and food add dataset --------
 
 #merge by week and snow grid
-forag <- merge(forag, dat, by = c("week", "year", "yearfactor"), all.x = TRUE)
+forag1 <- merge(forag, dat, by = c("week", "year", "yearfactor"), all.x = TRUE)
+setorder(forag1, id, date)
 
 #get controls only
-foragcon <- forag[food == 0]
+foragcon <- forag1[food == 0]
 
+
+
+# prep foraging data for conceptual diagram -------------------------------
+
+#cut to just 2018
+foragconcept <- forag1[year == 2018]
+
+#get the week prior by ID
+foragconcept[, weekprior := shift(week, 1, type = "lag"), id]
+
+#take difference between current week and last week
+foragconcept[, weekdiff := week - weekprior]
+
+#pull individuals that had a gap between weeks
+gapids <- foragconcept[weekdiff > 1, id]
+
+#remove those individuals
+foragconcept <- foragconcept[!id %in% gapids]
+
+#get length of weeks by id
+foragconcept[, sample := length(forage), id]
+
+#take only individuals with four or more weeks
+foragconcept <- foragconcept[sample > 3]
 
 
 
 # for conceptual diagram --------------------------------------------------
 
-(concept <- ggplot(forag[year == 2018])+
-  geom_path(aes(x = date, y = forage, group = id))+
-  #geom_point(aes(x = date, y = forage), shape = 1, size = 2)+
+(concept <- ggplot(foragconcept)+
+  geom_path(aes(x = week, y = forage, group = id))+
   labs(x = "Date", y = "Foraging effort (hr/day)")+
-  theme_minimal(base_size = 14))
-
-
+  theme_pubr(base_size = 16))
 
 
 
@@ -69,6 +92,19 @@ biomass <- ggplot(dat[year == 2017])+
 (fullplot <- ggarrange(fortemp, forbio, temp, biomass, ncol = 2, nrow = 2))
 
 
+
+# look at fecal protein vs foraging rate --------------------------
+
+fecal <- fecal[, .(CP = mean(CP_dm)), by = .(id, year, week)]
+
+fecfor <- merge(forag1, fecal, by = c("id", "year", "week"))
+
+ggplot(fecfor)+
+  geom_point(aes(x = forage, y = CP))+
+  geom_smooth(aes(x = forage, y = CP), method = "lm")
+
+summary(lm(CP ~ forage, fecfor))
+
+
 ggsave("Output/Figures/PostHoc_figure.jpeg", fullplot, width = 7, height = 7, unit = "in")
 ggsave("Output/Figures/conceptual_figure.jpeg", concept, width = 5, height = 5, unit = "in")
-
