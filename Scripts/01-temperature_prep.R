38c5246b04acb90611953e2ef066b0b92ac5e153
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2025-03-17 18:32:29 -0600

    add in conversion to metabolic rate

--- Scripts/01-temperature_prep.R
+++ Scripts/01-temperature_prep.R
@@ -23,15 +23,44 @@ weather <- rbindlist(ls.weather, fill = TRUE, use.names = TRUE)
 
 #take only the columns of interest and rename
 weather <- weather[, .SD, .SDcols = c(5, 14, 26, 10, 12)]
-names(weather) <- c("date", "tempmean", "snow_station", "tempmax", "tempmin")
+names(weather) <- c("date", "temp", "snow_station", "tempmax", "tempmin")
 
 #fix dates
 weather[, date := ymd(date)]
 weather[, m := month(date)]
 
-
 #cut to just january to march
 winter <- weather[m == 1|m == 2|m == 3]
 
-saveRDS(winter, "Output/Data/temperature_prepped.rds")
+#take just the date and mean temp
+winter <- winter[, .(date, temp)]
+
+
+
+# make relationship between temperature and  -----------------------------------------------
+
+#make a sequence of temperatures
+metab <- data.table(
+  temp = seq(-40, 15, 0.1)
+)
+
+#based on sheriff's paper, calculate metabolism based on ambient temp
+metab[temp > -10, VO := 976.68] #resting metabolic rate in the thermal neutral zone
+metab[temp <= -10, VO := 686.14 - 30.897*temp] #the increase in metabolic rate below thermal neutral zone
+
+#make a figure showing the relationship between temp and metabolism
+ggplot(metab)+
+  geom_line(aes(x = temp, y = VO), linewidth = 1)+
+  labs(x = "Ambient temperature (C)", y = "Oxygen consumption (ml/hr)")+
+  themepoints
+
+
+
+# final data and save -----------------------------------------------------
+
+#merge metabolic prediction with real temp data
+dat <- merge(winter, metab, by = "temp", all.x = TRUE)
+
+saveRDS(dat, "Output/Data/temperature_prepped.rds")
+
 
