0d66ecd4fca71037a7a4fbbefacac1330883c11c
Juliana Balluffi-Fry <jballuffi@gmail.com>
2025-03-20 12:26:12 -0600

    finalize results for foraging per hr

--- Extra scripts/foraging_per_hour.R
+++ Extra scripts/foraging_per_hour.R
@@ -25,131 +25,118 @@ foragcon <- forag[food == 0]
 foodyears <- forag[food == 1, unique(winter)]
 
 #take only females for food add comparisons
-foragfood <- forag[winter %in% foodyears & sex == "female"]
+foragfood <- forag[winter %in% foodyears]
 
 
 
-# correlation tests -------------------------------------------------------
+# difference between foraging per day and foraging per hour of nig --------
 
-cor(forag$biomass, forag$nightlength)
+ggplot(forag)+
+  geom_point(aes(x = nightlength, y = forage), alpha = 0.5)+
+  geom_smooth(aes(x = nightlength, y = forage), method = "lm", color = "black")+
+  labs(y = "Foraging effort (hr/day)", x = "Night length (hr)")+
+  theme_bw()
 
-cor(forag$biomass, forag$temp)
-cor(forag$biomass, forag$percap)
-cor(forag$percap, forag$temp)
-cor(forag$percap, forag$mortrate)
+ggplot(forag)+
+  geom_point(aes(x = nightlength, y = foragehr), alpha = 0.5)+
+  geom_smooth(aes(x = nightlength, y = foragehr), color = "black")+
+  labs(y = "Foraging effort (min/hr)", x = "Night length (hr)")+
+  theme_bw()
+
+ggplot(forag)+
+  geom_point(aes(x = forage, y = foragehr), alpha = 0.5)+
+  geom_smooth(aes(x = forage, y = foragehr), method = "lm", color = "black")+
+  labs(y = "Foraging effort (min/hr)", x = "Foraging effort (hr/day)")+
+  theme_bw()
 
 
 
 # AIC to explain foraging effort of controls only ------------------------
 
+#MODELS
 #models for controls only
-n <- lmer(foragehr ~ (1|id), foragcon) #null model
+n <- lmer(foragehr ~ 1 + (1|id), foragcon) #null model
 
 #single terms 
 S1 <- lmer(foragehr ~ biomass + (1|id), foragcon) #biomass food
-S2 <- lmer(foragehr ~ percap + (1|id), foragcon) #percapita food
+S2 <- lmer(foragehr ~ haredensity + (1|id), foragcon) #percapita food
 S3 <- lmer(foragehr ~ mortrate + (1|id), foragcon) #predation risk/mortality rate
 S4 <- lmer(foragehr ~ temp + (1|id), foragcon) #temperature
 
 #double terms 
 D1 <- lmer(foragehr ~ biomass + mortrate + (1|id), foragcon) #biomass and mortality
 D2 <- lmer(foragehr ~ biomass + temp + (1|id), foragcon) #biomass and temp
-D3 <- lmer(foragehr ~ percap + mortrate + (1|id), foragcon) #percap and mortality
-D4 <- lmer(foragehr ~ percap + temp + (1|id), foragcon) #percap and temp
+D3 <- lmer(foragehr ~ haredensity + mortrate + (1|id), foragcon) #percap and mortality
+D4 <- lmer(foragehr ~ haredensity + temp + (1|id), foragcon) #percap and temp
 D5 <- lmer(foragehr ~ mortrate + temp + (1|id), foragcon) #mortality and temp
+D6 <- lmer(foragehr ~ haredensity + biomass + (1|id), foragcon)
 
 #triple terms
-T1 <- lmer(foragehr ~ biomass + mortrate + temp + (1|id), foragcon)
-T2 <- lmer(foragehr ~ percap + mortrate + temp + (1|id), foragcon)
+T1 <- lmer(foragehr ~ biomass + temp + haredensity + (1|id), foragcon)
+T2 <- lmer(foragehr ~ biomass + mortrate + temp + (1|id), foragcon)
+T3 <- lmer(foragehr ~ biomass + haredensity + mortrate + (1|id), foragcon)
+T4 <- lmer(foragehr ~ haredensity + temp + mortrate + (1|id), foragcon)
+
+Q1 <- lmer(foragehr ~ haredensity + biomass + mortrate + temp + (1|id), foragcon)
+
 
 #list models
-mods <- list(n, S1, S2, S3, S4, D1, D2, D3, D4, D5, T1, T2)
-codes <- c("Null", "S1", "S2", "S3", "S4", "D1", "D2", "D3", "D4", "D5", "T1", "T2")
-vars <- c("None", "SB", "PCSB", "Mortality", "Temperature",
-          "SB + Mortality", "SB + Temperature", "PCSB + Mortality", "PCSB + Temperature", "Mortality + Temperature",
-          "SB + Mortality + Temperature", "PCSB + Mortality + Temperature")
+mods <- list(n, 
+             S1, S2, S3, S4, 
+             D1, D2, D3, D4, D5, D6, 
+             T1, T2, T3, T4, 
+             Q1)
+
+codes <- c("Null", 
+           "S1", "S2", "S3", "S4", 
+           "D1", "D2", "D3", "D4", "D5", "D6", 
+           "T1", "T2", "T3", "T4", 
+           "Q1")
 
 #use function from R/ folder to make an AIC table comparing all models.
 #function also extracts R2s
 AICcon <- make_aic_lmer(modlist = mods, modnames = codes)
 
-#make a dataframe with model codes and their designs
+
+#print formulas from list of models
+getform <- function(x){
+  as.character(formula(x)[3])
+}
+formulas <- lapply(mods, getform)
+formulas <- unlist(formulas)
+
+#make data table with model names and formulas
 moddesign <- data.table(
   Modnames = codes,
-  Variables = vars
+  Variables = formulas
 )
 
+#merge model designs with AIC table
 AICcon <- merge(moddesign, AICcon, by = "Modnames")
 setorder(AICcon, "Delta_AICc")
+
 
 
 
 # results of top model -------------------------------------------------------------
 
 #biomass and temperature
-summary(D2)
+summary(T1)
 
 #make predictive tables
-sb_pred <- as.data.table(ggpredict(D2, terms = c("biomass"))) #soluble biomass (sb)
-t_pred <- as.data.table(ggpredict(D2, terms = c("temp"))) #temperature (t)
+sb_pred <- as.data.table(ggpredict(T1, terms = c("biomass"))) #soluble biomass (sb)
+t_pred <- as.data.table(ggpredict(T1, terms = c("temp"))) #temperature (t)
+d_pred <- as.data.table(ggpredict(T1, terms = c("haredensity"))) #hare density 
 
-#get t-values
-sb_t = round(coef(summary(D2))[,"t value"][2], 2)
-t_t = round(coef(summary(D2))[,"t value"][3], 2)
-
-#get f-values
-D2anova <- anova(D2)
-sb_f <- round(D2anova$`F value`[1], 2)
-t_f <- round(D2anova$`F value`[2], 2)
-
-#get coefficients
-sb_coef <- round(fixef(D2)[2], 3)
-t_coef <- round(fixef(D2)[3], 3)
-
-#get standard errorts
-t_se <- round(se.fixef(D2)[3], 3)
-sb_se <- round(se.fixef(D2)[2], 3)
-
-
-
-# test top model on food add dataset ------------------------------------
-
-foodmod <- lmer(foragehr ~ biomass*food + temp*food + (1|id), foragfood) 
-summary(foodmod)
-
-#get t-values
-foodsb_t = round(coef(summary(foodmod))[,"t value"][2], 2)
-foodt_t = round(coef(summary(foodmod))[,"t value"][4], 2)
-food_t = round(coef(summary(foodmod))[,"t value"][3], 2)
-
-#get coefficients
-foodt_coef <- round(fixef(foodmod)[4], 3)
-food_coef <- round(fixef(foodmod)[3], 2)
-
-#get standard errorts
-foodt_se <- round(se.fixef(foodmod)[4], 3)
-food_se <- round(se.fixef(foodmod)[3], 2)
-
-#show effect of biomass*food
-foodb_pred <- as.data.table(ggpredict(foodmod, terms = c("biomass", "food")))
-setnames(foodb_pred, "group", "food")
-
-#show effect of temperature*food
-foodt_pred <- as.data.table(ggpredict(foodmod, terms = c("temp", "food")))
-setnames(foodt_pred, "group", "food")
-
-
-
-# Figure for controls and food add test -------------------------------------------
 
 #figure for foraging effort in response to soluble biomass
 (bfig <- 
-   ggplot()+
-   geom_point(aes(x = biomass, y = foragehr), alpha = .3, data = foragcon)+
-   geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = sb_pred)+
-   geom_line(aes(x = x, y = predicted), data = sb_pred)+
-   labs(x = NULL, y = "Foraging effort (min/hr)", subtitle = "A)")+
-   themepoints)
+    ggplot()+
+    geom_point(aes(x = biomass, y = foragehr), alpha = .3, data = foragcon)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = sb_pred)+
+    geom_line(aes(x = x, y = predicted), data = sb_pred)+
+    labs(x = "Twig biomass (kg/ha)", y = "Foraging effort (min/hr)")+
+    themepoints)
 
 #figures for foraging effort in response to temperature
 (tfig <- 
@@ -157,8 +144,42 @@ setnames(foodt_pred, "group", "food")
     geom_point(aes(x = temp, y = foragehr), alpha = .3, data = foragcon)+
     geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = t_pred)+
     geom_line(aes(x = x, y = predicted), data = t_pred)+
-    labs(x = NULL, y = NULL, subtitle = "B)")+
+    labs(x = "Temperature (C)", y = "Foraging effort (min/hr)")+
     themepoints)
+
+#figures for foraging effort in response to temperature
+(dfig <- 
+    ggplot()+
+    geom_point(aes(x = haredensity, y = foragehr), alpha = .3, data = foragcon)+
+    geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = .5, data = d_pred)+
+    geom_line(aes(x = x, y = predicted), data = d_pred)+
+    labs(x = "Hare density (hare/ha)", y = "Foraging effort (min/hr)")+
+    themepoints)
+
+
+
+
+
+# test top model on food add dataset ------------------------------------
+
+T1food <- lmer(foragehr ~ biomass*food + temp*food + haredensity*food + (1|id), foragfood)
+summary(T1food)
+
+#show effect of biomass*food
+foodb_pred <- as.data.table(ggpredict(T1food, terms = c("biomass", "food")))
+setnames(foodb_pred, "group", "food")
+
+#show effect of temperature*food
+foodt_pred <- as.data.table(ggpredict(T1food, terms = c("temp", "food")))
+setnames(foodt_pred, "group", "food")
+
+#hare density 
+foodd_pred <- as.data.table(ggpredict(T1food, terms = c("haredensity", "food")))
+setnames(foodd_pred, "group", "food")
+
+
+
+# Figure for controls and food add test -------------------------------------------
 
 #foraging rate of controls and food adds in response to food biomass
 (foodb_fig <- 
@@ -168,7 +189,7 @@ setnames(foodt_pred, "group", "food")
     geom_line(aes(x = x, y = predicted, color = food), data = foodb_pred)+
     scale_color_manual(values = foodcols, guide = NULL)+
     scale_fill_manual(values = foodcols, guide = NULL)+
-    labs(x = "Soluble biomass (kg/ha)", y = "Foraging effort (min/hr)", subtitle = "C)")+
+    labs(x = "Twig biomass (kg/ha)", y = "Foraging effort (min/hr)")+
     themepoints)
 
 #foraging rate of food adds and controls in response to temp
@@ -179,53 +200,21 @@ setnames(foodt_pred, "group", "food")
     geom_line(aes(x = x, y = predicted, color = food), data = foodt_pred)+
     scale_color_manual(values = foodcols, guide = NULL)+
     scale_fill_manual(values = foodcols, guide = NULL)+
-    labs(x = "Daily temperature (C)", y = NULL, subtitle = "D)")+
+    labs(x = "Daily temperature (C)", y = "Foraging effort (min/hr)")+
+    themepoints)
+
+(foodd_fig <- 
+    ggplot()+
+    geom_point(aes(x = haredensity, y = foragehr, color = food), alpha = .2, data = foragfood)+
+    geom_ribbon(aes(x = x, ymax = conf.high, ymin = conf.low, fill = food), alpha = .5, data = foodd_pred)+
+    geom_line(aes(x = x, y = predicted, color = food), data = foodd_pred)+
+    scale_color_manual(values = foodcols, guide = NULL)+
+    scale_fill_manual(values = foodcols, guide = NULL)+
+    labs(x = "Hare density (hare/ha)", y = "Foraging effort (min/hr)")+
     themepoints)
 
 #make full paneled figure
-fullfig <- ggarrange(bfig, tfig, foodb_fig, foodt_fig, nrow = 2, ncol = 2)
+fullfig <- ggarrange(foodb_fig, foodt_fig, foodd_fig, nrow = 3, ncol = 2)
 
-
-plot(forag$forage ~ forag$week)
-
-
-# percap result -----------------------------------------------------------
-
-summary(D4)
-pc_pred <- as.data.table(ggpredict(D4, terms = "percap"))
-
-ggplot(foragcon)+
-  geom_point(aes(x = percap, y = foragehr), alpha = 0.2)+
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = 0.5, data = pc_pred)+
-  geom_line(aes(x = x, y = predicted), data = pc_pred)+
-  labs(x = "Hare density (hares/ha)", y = "Foraging effort (min/hr)")+
-  themepoints
-
-
-
-# look at hare density instead --------------------------------------------
-
-test1 <- lmer(foragehr ~ haredensity + (1|id), foragcon)
-test1dt <- as.data.table(ggpredict(test1, terms = "haredensity"))
-
-ggplot(foragcon)+
-  geom_point(aes(x = haredensity, y = foragehr), alpha = 0.2)+
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high), alpha = 0.5, data = test1dt)+
-  geom_line(aes(x = x, y = predicted), data = test1dt)+
-  labs(x = "Hare density (hares/ha)", y = "Foraging effort (min/hr)")+
-  themepoints
-
-test2 <- lmer(foragehr ~ haredensity*food + (1|id), foragfood)
-test2dt <- as.data.table(ggpredict(test2, terms = c("haredensity", "food")))
-setnames(test2dt, "group", "food")
-
-ggplot(foragfood)+
-  geom_point(aes(x = haredensity, y = foragehr, color = food), alpha = .2, data = foragfood)+
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, fill = food), alpha = 0.5, data = test2dt)+
-  geom_line(aes(x = x, y = predicted, color = food), data = test2dt)+
-  scale_color_manual(values = foodcols, guide = NULL)+
-  scale_fill_manual(values = foodcols, guide = NULL)+
-  labs(x = "Hare density (hares/ha)", y = "Foraging effort (min/hr)")+
-  themepoints
 
 
