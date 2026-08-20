c3866a243f1b7d55e917b0de40abdd1d2aeeb7e5
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2026-08-20 18:36:45 -0400

    save final results and remove old AIC

--- Scripts/08-results_nonAIC.R
+++ Scripts/08-results_nonAIC.R
@@ -22,15 +22,15 @@ Q1 <- lmer(CP_dm ~ biomass*food + temp*food + haredensity*food + mortrate*food +
 
 #summarize model
 summary(Q1)
-anova(Q1) #gives f-value. Typically f = 4 is significant.
-car::Anova(Q1) #gives p-values
-lmerTest::ranova(Q1)
+anova(Q1) #gives f value
+Anova(Q1) #gives p value
+lmerTest::ranova(Q1) #residuals
+
+#get table of results
+Q1_sum <- lmer_out(Q1)
 
 #get R2
 Q1R2 <- round(r.squaredGLMM(Q1), 2)[1]
-
-#Make table for model output using lmer_out function
-Q1_sum <- lmer_out(Q1)
 
 #make predictive table
 biopred_fecal <- as.data.table(ggpredict(Q1, terms = c("biomass", "food")))
@@ -78,10 +78,15 @@ Q2 <- lmer(forage ~ haredensity*food + biomass*food + mortrate*food + temp*food 
 
 #model summaries
 summary(Q2)
-anova(Q2)
-car::Anova(Q2)
-lmerTest::ranova(Q2)
-round(r.squaredGLMM(Q2), 2)[1]
+anova(Q2) #fvalues
+Anova(Q2)[3] #pvalues
+lmerTest::ranova(Q2) #residuals
+
+#get table of results
+Q2_sum <- lmer_out(Q2)
+
+#get R2
+Q2R2 <- round(r.squaredGLMM(Q2), 2)[1]
 
 #get prediction for density*food
 densitypred_forage <- as.data.table(ggpredict(Q2, terms = c("haredensity", "food")))
@@ -128,7 +133,9 @@ foragefig
 ggsave("Output/Figures/Fecal_Figure.jpeg", fecalfig, width = 4, height = 8, unit = "in")
 ggsave("Output/Figures/Foraging_Figure.jpeg", foragefig, width = 4, height = 8, unit = "in")
 
+write.csv(Q1_sum, "Output/Tables/fecal_model_results.csv")
+write.csv(Q2_sum, "Output/Tables/foraging_model_results.csv")
 
-
-
+saveRDS(Q1R2, "Output/Data/fecal_R2.rds")
+saveRDS(Q2R2, "Output/Data/foraging_R2.rds")
 
