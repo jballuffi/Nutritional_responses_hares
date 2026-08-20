c7909ffafcc5af904e936b85f5fb1fd14354449b
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2026-08-20 18:36:16 -0400

    fix error

--- R/function_lmer_out.R
+++ R/function_lmer_out.R
@@ -6,12 +6,12 @@ lmer_out <- function(model) {
   
   #start table of effect names and coef +/- standard error
   modcoef <- data.table(
-    Effect = names(fixef(Q1)),
+    Effect = names(fixef(model)),
     Slope = paste0(coef, " ± ", se)
     )
   
   modcoef <- modcoef[!grep("Inter", Effect)] #remove intercept
-  modcoef[, `F-value` := round(anova(Q1)[4], 2)] #add F value
+  modcoef[, `F-value` := round(anova(model)[4], 2)] #add F value
   modcoef[, `p-value` := round(Anova(model)[3], 3)] #add p values
   
   return(modcoef) #return full table
